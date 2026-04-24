3d24028949915a56a74dd327156e8932229f87c6
Егор Ленин <edlenin07@gmail.com>
2026-04-25 00:04:26 +0300

    написал тест на проверку начального состояния игрового поля

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,34 @@
-namespace TestProject1
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
+namespace TicTacToe.Tests
+{
+    public class GameTests
+    {
+        // ==========================================
+        // 1. Инициализация и сброс состояния
+        // ==========================================
+
+        /// <summary>
+        /// Тест: Проверка начального состояния игрового поля.
+        /// Ожидание: Все ячейки пусты (содержат пустую строку).
+        /// </summary>
+        [Fact]
+        public void NewGame_BoardIsEmpty()
+        {
+            // Arrange
+            var game = new Game();
+
+            // Act & Assert
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Assert.Equal("", game.Board[i][j]);
+                }
+            }
         }
     }
 }
